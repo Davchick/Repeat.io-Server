450cd04a6e4f3f2c1586e01d782285d232da99ce
Davchick <davchick2000ort@gmail.com>
2024-01-03 15:41:53 +0300

    Create a server and connect to the mongoDB

---
+++ server.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import express from "express";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+
+/* CONFIGURATIONS */
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(express.json());
+app.use(morgan("common"));
+dotenv.config();
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 3000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server works on port - ${PORT}`))
+  )
+  .catch((err) => console.log(err));
