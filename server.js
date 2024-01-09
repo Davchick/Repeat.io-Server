91ee6f21d70f27f69ff3bef7096c3ac62c11f849
Davchick <davchick2000ort@gmail.com>
2024-01-09 14:48:59 +0300

    add routing function

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+import { fileURLToPath } from "url";
 import mongoose from "mongoose";
 import express from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
 
 /* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(express.json());
 app.use(morgan("common"));
+app.use(cors());
 dotenv.config();
+
+/* ROUTING */
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+});
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3000;
