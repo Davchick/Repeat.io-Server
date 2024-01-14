4ea6ecddae28f36e698cd2ee18ccef30f7fa88cc
Davchick <davchick2000ort@gmail.com>
2024-01-14 20:42:42 +0300

    add register functionality

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-import { fileURLToPath } from "url";
 import mongoose from "mongoose";
 import express from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
-import path from "path";
 
 /* CONFIGURATIONS */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -17,11 +13,6 @@ app.use(express.json());
 app.use(morgan("common"));
 app.use(cors());
 dotenv.config();
-
-/* ROUTING */
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-});
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3000;
